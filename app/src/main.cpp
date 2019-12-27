9f6dcea04177fb65c45e01896ffc5ed082010182
Hussein Taher <hussein_taher@outlook.com>
2019-12-27 23:52:43 +0100

    added open file dialog

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,7 +1,9 @@
 ﻿#include <iostream>
 #include <thread>
 
+#include <ShObjIdl.h>
 #include <Windows.h>
+#include <atlbase.h>
 
 #include "window/MainWindow.h"
 
@@ -28,6 +30,30 @@ int main() {
 		while (PeekMessage(&msg, window.Window(), NULL, NULL, PM_REMOVE)) {
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
+		}
+
+		CComPtr<IFileOpenDialog> pFileOpen;
+		hr = pFileOpen.CoCreateInstance(__uuidof(FileOpenDialog));
+
+		if (SUCCEEDED(hr)) {
+			CComQIPtr<IFileDialogCustomize> pFileDialogCustomize(pFileOpen);
+			if (pFileDialogCustomize) {
+				pFileDialogCustomize->AddText(10001, L"안녕하세요 여러분, 트와이스입니다!");
+			}
+
+			hr = pFileOpen->Show(NULL);
+			if (SUCCEEDED(hr)) {
+				CComPtr<IShellItem> pItem;
+				hr = pFileOpen->GetResult(&pItem);
+				if (SUCCEEDED(hr)) {
+					PWSTR pszFilePath;
+					hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+					if (SUCCEEDED(hr)) {
+						MessageBoxW(NULL, pszFilePath, L"File Path", MB_OK);
+						CoTaskMemFree(pszFilePath);
+					}
+				}
+			}
 		}
 
 		std::this_thread::yield();
