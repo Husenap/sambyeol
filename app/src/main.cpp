9232c5c3f4092d56edde61d69aa53efe46dd7fc8
Hussein Taher <Husenap@users.noreply.github.com>
2019-12-27 23:09:46 +0100

    Feature/window (#1)
    * updated repository from template
    
    * basic window
    
    * updated basic window handling to have a peekmessage loop

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,6 +1,39 @@
 ﻿#include <iostream>
+#include <thread>
+
+#include <Windows.h>
+
+#include "window/MainWindow.h"
 
 int main() {
-	std::cout << "Annyeong Segye!" << std::endl;
+	MainWindow window;
+
+	HRESULT hr;
+
+	hr = CoInitializeEx(NULL, COINIT_MULTITHREADED | COINIT_DISABLE_OLE1DDE | COINIT_SPEED_OVER_MEMORY);
+	if (FAILED(hr)) {
+		std::cout << "Failed to initialize the COM library" << std::endl;
+		return hr;
+	}
+
+	if (!window.Create("SamByeol", WS_OVERLAPPEDWINDOW, 0, 0, 0, 500, 500)) {
+		std::cout << "Failed to create window!" << std::endl;
+		return 1;
+	}
+	ShowWindow(window.Window(), SW_SHOW);
+
+	MSG msg = {};
+
+	while (window.IsOpen()) {
+		while (PeekMessage(&msg, window.Window(), NULL, NULL, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		std::this_thread::yield();
+	}
+
+	CoUninitialize();
+
 	return 0;
 }
