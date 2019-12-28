44f09b18b3228de846e9a5acaba03afd2ee29413
Hussein Taher <Husenap@users.noreply.github.com>
2019-12-28 04:32:48 +0100

    static msvc runtime

--- cmake/compiler_features.cmake
+++ cmake/compiler_features.cmake
@@ -1,7 +1,7 @@
 if (MSVC)
 	# Explicitly specify flags in order to enable VS2017 C++17 features.
 	# In VS2017 15.9.3 this is needed to correctly identify nested namespace definitions
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /std:c++17")
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /std:c++17 /MT")
 	add_definitions(-DUNICODE -D_UNICODE -DNOMINMAX -DWIN_LEAN_AND_MEAN)
     set(CMAKE_EXE_LINKER_FLAGS_RELEASE "${CMAKE_EXE_LINKER_FLAGS} /SUBSYSTEM:WINDOWS")
 else()
