09b64106cc96368b30906809298c47c3ab10befa
Hussein Taher <hussein_taher@outlook.com>
2019-12-29 02:41:29 +0100

    updated minimum cmake version

--- cmake/compiler_features.cmake
+++ cmake/compiler_features.cmake
@@ -3,7 +3,7 @@ if (MSVC)
 	# In VS2017 15.9.3 this is needed to correctly identify nested namespace definitions
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /std:c++17")
 	add_definitions(-DUNICODE -D_UNICODE -DNOMINMAX -DWIN_LEAN_AND_MEAN)
-    set(CMAKE_EXE_LINKER_FLAGS_RELEASE "${CMAKE_EXE_LINKER_FLAGS} /SUBSYSTEM:WINDOWS")
+    set(CMAKE_EXE_LINKER_FLAGS_RELEASE "${CMAKE_EXE_LINKER_FLAGS} /SUBSYSTEM:WINDOWS /MT")
 else()
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++1z")
 endif()
